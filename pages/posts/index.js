3a7601dca6cb467f2675e0a376926cea088ab25e
VishalGithub11 <modanwalvishal@gmail.com>
2022-07-25 22:03:01 +0530

    fallback true added

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -22,7 +22,7 @@ export default PostList
 
 export async function getStaticProps(){
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-
+ 
     const data = await response.json();
 
     return {
