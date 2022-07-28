03d4bca694bb7d30aea8475fc909e865c8511368
VishalGithub11 <modanwalvishal@gmail.com>
2022-07-28 15:15:28 +0530

    redirects

--- next-misc/next.config.js
+++ next-misc/next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  //suppose a user go to old blog whose url is like '/old_blog/path' and now you want to user who goes on this url, redirect to some new url '/new_blog/path' use redirects below:-------
+  // redirects: async()=>{
+  //   return [
+  //     {
+  //       source: '/onlyFooter',
+  //       destination: '/',
+  //       permanent:true //status code 308, 307 for false
+  //     }
+  //   ]
+  // }
 }
 
 module.exports = nextConfig
